33449d1ade7da37b0f30965733f1ebbbb64f1b77
Frankyston Lins <frankyston@gmail.com>
2020-10-05 11:27:01 -0300

    Adding locales

--- config/application.rb
+++ config/application.rb
@@ -6,10 +6,27 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+def set_locale_configs
+  # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
+  # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+  config.time_zone = 'Brasilia'
+  # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+  # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+  config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**/*.{rb,yml}').to_s]
+  config.i18n.available_locales = ["pt-BR"]
+  config.i18n.default_locale = "pt-BR"
+
+  # formats date and datetime default
+  Time::DATE_FORMATS[:default] = "%d/%m/%Y %H:%M"
+  Date::DATE_FORMATS[:default] = "%d/%m/%Y"
+end
+
 module ChallengeSurvey
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+
+    set_locale_configs
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
